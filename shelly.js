c2a33d8c2633c68d98cf3dd83ba5458d1ba2eef8
Aman Shabbas <83913651+Aman-shabbas@users.noreply.github.com>
2024-12-20 10:17:18 +0530

    completed echo

--- shelly.js
+++ shelly.js
@@ -1,17 +1,6 @@
 const runEcho = function (args) {
-  if (args.join(" ") === "shrutika") {
-    return "idiot bosale"
-  }
-  if (args.join(" ") === "aman") {
-    return "***mandan poda pull vala ninnk buddhi iill stupid mad bavlat murkh mand pagal*** ";
-  }
-  if (args.join(" ") === "sujoy") {
-    return "***aman ka chamcha*** ";
-  }
-  if (args.join(" ") === "anushri" || args.join(" ") === "crunchi") {
-    return "crunchi"
-  }
-  return args.join(" ");
+  const message = args.join(" ").replaceAll("\"", "");
+  return message;
 }
 
 const runCommand = function (commandString) {
